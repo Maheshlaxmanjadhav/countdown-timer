6a9008a60e7d78d605c2b76ec646052af77394bd
Mahesh Jadhav <95060759+Maheshlaxmanjadhav@users.noreply.github.com>
2022-01-27 12:32:15 +0530

    Add files via upload

---
+++ color/color.js
@@ -0,0 +1,244 @@
+function fun(){
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun1(){
+    var copyText = document.getElementById("myInput1");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun2(){
+    var copyText = document.getElementById("myInput2");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun3(){
+    var copyText = document.getElementById("myInput3");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun4(){
+    var copyText = document.getElementById("myInput4");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun5(){
+    var copyText = document.getElementById("myInput5");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun6(){
+    var copyText = document.getElementById("myInput6");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun7(){
+    var copyText = document.getElementById("myInput7");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun8(){
+    var copyText = document.getElementById("myInput8");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun9(){
+    var copyText = document.getElementById("myInput9");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun10(){
+    var copyText = document.getElementById("myInput10");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+
+
+function fun11(){
+    var copyText = document.getElementById("myInput11");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun12(){
+    var copyText = document.getElementById("myInput12");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun13(){
+    var copyText = document.getElementById("myInput13");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun14(){
+    var copyText = document.getElementById("myInput14");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun15(){
+    var copyText = document.getElementById("myInput15");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun16(){
+    var copyText = document.getElementById("myInput16");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun17(){
+    var copyText = document.getElementById("myInput17");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun18(){
+    var copyText = document.getElementById("myInput18");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun19(){
+    var copyText = document.getElementById("myInput19");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun20(){
+    var copyText = document.getElementById("myInput20");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+
+function fun21(){
+    var copyText = document.getElementById("myInput21");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun22(){
+    var copyText = document.getElementById("myInput22");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun23(){
+    var copyText = document.getElementById("myInput23");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun24(){
+    var copyText = document.getElementById("myInput24");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun25(){
+    var copyText = document.getElementById("myInput25");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun26(){
+    var copyText = document.getElementById("myInput26");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun27(){
+    var copyText = document.getElementById("myInput27");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun28(){
+    var copyText = document.getElementById("myInput28");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun29(){
+    var copyText = document.getElementById("myInput29");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun30(){
+    var copyText = document.getElementById("myInput30");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+
+function fun31(){
+    var copyText = document.getElementById("myInput31");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun32(){
+    var copyText = document.getElementById("myInput32");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun33(){
+    var copyText = document.getElementById("myInput33");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun34(){
+    var copyText = document.getElementById("myInput34");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun35(){
+    var copyText = document.getElementById("myInput35");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun36(){
+    var copyText = document.getElementById("myInput36");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun37(){
+    var copyText = document.getElementById("myInput37");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun38(){
+    var copyText = document.getElementById("myInput38");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
+function fun39(){
+    var copyText = document.getElementById("myInput39");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("You have copied the colour " + copyText.value +" to your Clipboard");
+}
