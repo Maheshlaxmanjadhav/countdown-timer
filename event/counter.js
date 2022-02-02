7deb3ca8801b1fef5fb1cfeafc913efe16924ef0
Mahesh Jadhav <95060759+Maheshlaxmanjadhav@users.noreply.github.com>
2022-02-02 17:19:03 +0530

    Add files via upload

--- event/counter.js
+++ event/counter.js
@@ -1,5 +1,8 @@
 function countdown()
 {
+	var x=document.getElementById("ename").value;
+	document.getElementById("eve").innerHTML=x+" Event Starts on";
+
 	var v1=document.getElementById("cdate").value;
 	console.log(v1);
 
@@ -21,11 +24,12 @@ function countdown()
 	var s = Math.floor((diff%(1000*60))/(1000));
 	console.log(s);
 
-	document.getElementById("days").innerHTML=d;
-	document.getElementById("hours").innerHTML=h;
-	document.getElementById("minutes").innerHTML=m;
-	document.getElementById("seconds").innerHTML=s;
+	document.getElementById("days").innerHTML=d+"d";
+	document.getElementById("hours").innerHTML=h+"H";
+	document.getElementById("minutes").innerHTML=m+"M";
+	document.getElementById("seconds").innerHTML=s+"S";
 }
 function callme(){
 	document.getElementById("dis").style.display="block";
+	document.getElementById("container").style.display="none";
 	setInterval(countdown,1000);}
